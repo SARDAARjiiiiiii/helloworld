52ea46029ac139e0f059fcdfdf34f6c694177911
Rajwinder Singh <you@example.com>
2022-09-16 23:53:52 -0400

    init commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Rajwinder SIngh Student ID: 148704208 Date: 16 September, 2022
+*
+*  Online (Cyclic) URL:  https://nice-cyan-kingfisher-kilt.cyclic.app/
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
